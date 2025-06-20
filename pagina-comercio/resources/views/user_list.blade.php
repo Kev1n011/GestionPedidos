8648cd65fa457f53eb5f8084ab95975a5b9222e2
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2025-06-20 16:40:06 -0700

    Lista de usuarios: Obtener usuarios (llenado de tabla)

--- pagina-comercio/resources/views/user_list.blade.php
+++ pagina-comercio/resources/views/user_list.blade.php
@@ -1,8 +1,3 @@
-<?php
-
- var_dump($users);
-?>
-
 <!doctype html>
 <html lang="en">
 <!-- [Head] start -->
@@ -522,181 +517,52 @@
                     <tr>
                       <th>Name</th>
                       <th>Position</th>
-                      <th>Office</th>
-                      <th>Age</th>
-                      <th>Start date</th>
+                      <th>Email</th>
+                      <th>Phone Number</th>
+                      <th>Created at</th>
                       <th>Status</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>
-                        <div class="d-inline-block align-middle">
-                          <img src="../assets/images/user/avatar-1.jpg" alt="user image"
-                            class="img-radius align-top m-r-15" style="width: 40px" />
-                          <div class="d-inline-block">
-                            <h6 class="m-b-0">Quinn Flynn</h6>
-                            <p class="m-b-0 text-primary">Android developer</p>
+                    @foreach ($users['data'] as $user)
+                      <tr>
+                          @php
+                              $date = new DateTime($user['created_at']);
+                              $newDate = $date->format('Y-m-d'); 
+
+                              $phone_number = $user['phone_number'];
+                              if($user['phone_number'] == null){
+                                $phone_number = "Sin numero";
+                              }
+                          @endphp
+                        <td>
+                          <div class="d-inline-block align-middle">
+                            <img src={{asset ("/assets/images/user/avatar-1.jpg")}} alt="user image"
+                              class="img-radius align-top m-r-15" style="width: 40px" />
+                            <div class="d-inline-block">
+                              <h6 class="m-b-0">{{ $user['name'] }}</h6>
+                              <p class="m-b-0 text-primary">{{ $user['role'] }}</p>
+                            </div>
                           </div>
-                        </div>
-                      </td>
-                      <td>Support Lead</td>
-                      <td>Edinburgh</td>
-                      <td>61</td>
-                      <td>2011/04/25</td>
-                      <td>
-                        <span class="badge bg-light-success">Active</span>
-                        <div class="overlay-edit">
-                          <ul class="list-inline mb-0">
-                            <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
-                                  class="ti ti-pencil f-18"></i></a></li>
-                            <li class="list-inline-item m-0"><a href="#"
-                                class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-inline-block align-middle">
-                          <img src="../assets/images/user/avatar-2.jpg" alt="user image"
-                            class="img-radius align-top m-r-15" style="width: 40px" />
-                          <div class="d-inline-block">
-                            <h6 class="m-b-0">Garrett Winters</h6>
-                            <p class="m-b-0 text-primary">Android developer</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>63</td>
-                      <td>2011/07/25</td>
-                      <td>
-                        <span class="badge bg-light-danger">Disabled</span>
-                        <div class="overlay-edit">
-                          <ul class="list-inline mb-0">
-                            <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
-                                  class="ti ti-pencil f-18"></i></a></li>
-                            <li class="list-inline-item m-0"><a href="#"
-                                class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-inline-block align-middle">
-                          <img src="../assets/images/user/avatar-3.jpg" alt="user image"
-                            class="img-radius align-top m-r-15" style="width: 40px" />
-                          <div class="d-inline-block">
-                            <h6 class="m-b-0">Ashton Cox</h6>
-                            <p class="m-b-0 text-primary">Android developer</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>Junior Technical Author</td>
-                      <td>San Francisco</td>
-                      <td>66</td>
-                      <td>2009/01/12</td>
-                      <td>
-                        <span class="badge bg-light-danger">Disabled</span>
-                        <div class="overlay-edit">
-                          <ul class="list-inline mb-0">
-                            <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
-                                  class="ti ti-pencil f-18"></i></a></li>
-                            <li class="list-inline-item m-0"><a href="#"
-                                class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-inline-block align-middle">
-                          <img src="../assets/images/user/avatar-4.jpg" alt="user image"
-                            class="img-radius align-top m-r-15" style="width: 40px" />
-                          <div class="d-inline-block">
-                            <h6 class="m-b-0">Cedric Kelly</h6>
-                            <p class="m-b-0 text-primary">Android developer</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>Senior Javascript Developer</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2012/03/29</td>
-                      <td>
-                        <span class="badge bg-light-success">Active</span>
-                        <div class="overlay-edit">
-                          <ul class="list-inline mb-0">
-                            <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
-                                  class="ti ti-pencil f-18"></i></a></li>
-                            <li class="list-inline-item m-0"><a href="#"
-                                class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-inline-block align-middle">
-                          <img src="../assets/images/user/avatar-4.jpg" alt="user image"
-                            class="img-radius align-top m-r-15" style="width: 40px" />
-                          <div class="d-inline-block">
-                            <h6 class="m-b-0">Airi Satou</h6>
-                            <p class="m-b-0 text-primary">Android developer</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>33</td>
-                      <td>2008/11/28</td>
-                      <td>
-                        <span class="badge bg-light-success">Active</span>
-                        <div class="overlay-edit">
-                          <ul class="list-inline mb-0">
-                            <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
-                                  class="ti ti-pencil f-18"></i></a></li>
-                            <li class="list-inline-item m-0"><a href="#"
-                                class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-inline-block align-middle">
-                          <img src="../assets/images/user/avatar-5.jpg" alt="user image"
-                            class="img-radius align-top m-r-15" style="width: 40px" />
-                          <div class="d-inline-block">
-                            <h6 class="m-b-0">Brielle Williamson</h6>
-                            <p class="m-b-0 text-primary">Android developer</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>Integration Specialist</td>
-                      <td>New York</td>
-                      <td>61</td>
-                      <td>2012/12/02</td>
-                      <td>
-                        <span class="badge bg-light-danger">Disabled</span>
-                        <div class="overlay-edit">
-                          <ul class="list-inline mb-0">
-                            <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
-                                  class="ti ti-pencil f-18"></i></a></li>
-                            <li class="list-inline-item m-0"><a href="#"
-                                class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
+                        </td>
+                        <td>{{ $user['role'] }}</td>
+                        <td>{{ $user['email'] }}</td>
+                        <td>{{ $phone_number }}</td>
+                        <td>{{$newDate}}</td>
+                        <td>
+                         <ul class="list-inline mb-0">
+                              <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-secondary"><i
+                                    class="ti ti-eye f-18"></i></a></li>
+                              <li class="list-inline-item m-0"><a href="#" class="avtar avtar-s btn btn-primary"><i
+                                    class="ti ti-pencil f-18"></i></a></li>
+                              <li class="list-inline-item m-0"><a href="#"
+                                  class="avtar avtar-s btn bg-white btn-link-danger"><i class="ti ti-trash f-18"></i></a>
+                              </li>
+                            </ul>
+                        </td>
+                      </tr>
+
+                    @endforeach
                   </tbody>
                 </table>
               </div>
@@ -722,7 +588,7 @@
     });
   </script>
   <!-- [Page Specific JS] end -->
-  
+
 
 </body>
 <!-- [Body] end -->
